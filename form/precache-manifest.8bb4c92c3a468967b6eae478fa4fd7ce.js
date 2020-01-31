a397d836654c313ee66213fc8e266ff643333fff
zhangzhifeng <zhangzhifeng@moseeker.com>
2020-01-31 22:09:45 +0800

    deploy form

---
+++ form/precache-manifest.8bb4c92c3a468967b6eae478fa4fd7ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c27ddfa05f7b517d6c4e557747086eb",
+    "url": "/static/media/time.2c27ddfa.less"
+  },
+  {
+    "revision": "7930be8a4171b832a7eff6fd7a4f7060",
+    "url": "/static/media/display.7930be8a.less"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1ff09c5e870802e86cc",
+    "url": "/static/js/main.e1ff09c5.chunk.js"
+  },
+  {
+    "revision": "52b0bf4ff28bcc5cbbce",
+    "url": "/static/js/1.52b0bf4f.chunk.js"
+  },
+  {
+    "revision": "e1ff09c5e870802e86cc",
+    "url": "/static/css/main.1c23c439.chunk.css"
+  },
+  {
+    "revision": "52b0bf4ff28bcc5cbbce",
+    "url": "/static/css/1.d04ede7b.chunk.css"
+  },
+  {
+    "revision": "373e9b6bd9845dbd3bb79b45bf1705eb",
+    "url": "/index.html"
+  }
+];
